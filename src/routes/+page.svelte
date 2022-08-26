22fc9f85137f753046b11045cec3020fb1c0ecc3
H7KZ <honzik.kominek@gmail.com>
2022-08-26 19:34:09 +0200

    Initial commit v2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,2 @@
-<script lang="ts">
-    import Xd from "@components/xd.svelte";
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Xd />
--- svelte.config.js
+++ svelte.config.js
@@ -1,40 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
-import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			scss: {
-				prependData: '@use "src/variables.scss" as *;'
-			}
-		})
-	],
+	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-
-		vite: {
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: '@use "src/variables.scss" as *;'
-					}
-				}
-			},
-			resolve: {
-				alias: {
-					// these are the aliases and paths to them
-					'@lib': path.resolve('./src/lib'),
-					'@components': path.resolve('./src/lib/components'),
-					'@data': path.resolve('./src/lib/data'),
-					'@functions': path.resolve('./src/lib/functions')
-				}
-			}
-		}
+		adapter: adapter()
 	}
 };
 
