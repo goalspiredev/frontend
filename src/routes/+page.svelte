7c40091bab2c65e12b38c5464033a079ad87c813
H7KZ <74021016+H7KZ@users.noreply.github.com>
2022-09-22 11:56:09 +0200

    Default Page (#1)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <h1>
+        Site is being built
+    </h1>
+    <img src="https://c.tenor.com/58DNSt-Lvw0AAAAd/corgi-computer.gif" alt="corgi pc gif">
+    <h1>
+        Check back later
+    </h1>
+</div>
+
+<style scoped lang="scss">
+    div {
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+
+        font-family: 'Comfortaa', sans-serif;
+
+        h1 {
+            font-size: 1.75rem;
+            font-weight: 800;
+
+            @media screen and (min-width: 768px) {
+                font-size: 2.5rem;
+            }
+        }
+
+        img {
+            width: 16rem;
+
+            @media screen and (min-width: 768px) {
+                width: 24rem;
+            }
+        }
+    }
+</style>
