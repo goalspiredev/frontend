30934730ea9a14f63273a77163d4eccccd1d3d07
Seb <seb1rblx@gmail.com>
2022-11-12 15:55:21 +0100

    Finish up scrolling cards component

--- src/routes/scrolling/+page.svelte
+++ src/routes/scrolling/+page.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
     import {inview} from 'svelte-inview';
+    import ExampleCard from "../../lib/components/cards/ExampleCard.svelte";
+    import {onMount} from "svelte";
 
-    let inviewOptions= {
+    let inviewOptions = {
         unobserveOnEnter: true
-    }
-
-    let deltaLimits = {
-        up: -1000,
-        down: 1000
     }
 
     let totalDelta = 0;
     let currentTarget = null;
 
+    let currentCard = null;
+    let allChildCards = [];
+    let currentCardIndex = 0;
+
+    function registerChildCard(card) {
+        allChildCards.push(card);
+    }
+
     function lockCard(event) {
-        console.log(event);
-        document.body.style.overflowY = "hidden";
+        document.body.style.overflow = "hidden";
 
         // scroll to the parent
         event.target.parentElement.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
@@ -27,21 +31,64 @@
     function unlockCard() {
         document.removeEventListener("wheel", onScroll);
         document.body.style.overflowY = "scroll";
+        currentCard.parentNode.removeChild(currentCard);
     }
 
     function onScroll(event) {
         totalDelta += event.deltaY;
-        currentTarget.parentElement.style.backgroundColor = `rgb(${totalDelta/5}, ${totalDelta/5}, ${totalDelta/5})`;
-        if (totalDelta > deltaLimits.down || totalDelta < deltaLimits.up) {
+        currentCard.style.transform = `translateX(${totalDelta}px)`;
+        // if (totalDelta > deltaLimits.down || totalDelta < deltaLimits.up) {
+        //    unlockCard();
+        // }
+
+        // when the card is scrolled out of the screen, remove it
+        if (totalDelta > window.innerWidth + 350 || totalDelta < -window.innerWidth) {
+            // unlockCard();
+            switchCardContext(currentCardIndex);
+        }
+    }
+
+    function switchCardContext(index)
+    {
+        console.log(allChildCards.length);
+        totalDelta = 0;
+        currentCard.parentNode.removeChild(currentCard);
+
+        if (index < allChildCards.length - 1) {
+            currentCardIndex++;
+            currentCard = allChildCards[currentCardIndex];
+        } else {
+            currentCardIndex = 0;
             unlockCard();
         }
     }
+
+    onMount(async () => {
+        //wait for currentCard to not be null
+        while (allChildCards[0] === null) {
+            await new Promise(resolve => setTimeout(resolve, 100));
+        }
+        currentCard = allChildCards[0];
+        for (let card of allChildCards)
+        {
+            card.style.left = "-350px";
+        }
+    })
 </script>
 
 <div class="container">
     <div class="card2">
     </div>
     <div class="card1">
+        <div class="cardWrapper" bind:this={allChildCards[0]}>
+            <ExampleCard name="1" bgColor="red"/>
+        </div>
+        <div class="cardWrapper" bind:this={allChildCards[1]}>
+            <ExampleCard name="2" bgColor="green"/>
+        </div>
+        <div class="cardWrapper" bind:this={allChildCards[2]}>
+            <ExampleCard name="3" bgColor="blue"/>
+        </div>
         <div class="onViewTriggerPoint" use:inview={inviewOptions} on:enter={(a) => lockCard(a)}></div>
     </div>
     <div class="card2"></div>
@@ -61,6 +108,17 @@
       width: 100%;
       height: 100vh;
       position: relative;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      .cardWrapper{
+        position: absolute;
+        width: auto;
+        height: auto;
+      }
 
       .onViewTriggerPoint {
         // In the middle of the card, not affecting any other elements
