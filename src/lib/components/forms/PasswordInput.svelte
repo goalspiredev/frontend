20879a8f406d13dd52b91c1a5ffe1c74064f0680
Seb <seb1rblx@gmail.com>
2022-11-19 22:11:35 +0100

    Add TextInput and PasswordInput animations

--- src/lib/components/forms/PasswordInput.svelte
+++ src/lib/components/forms/PasswordInput.svelte
@@ -17,13 +17,30 @@
 		passwordShown = !passwordShown;
 		passwordInput.type = passwordShown ? 'text' : 'password';
 	}
+
+	let labelText: HTMLElement;
+	let parent: HTMLElement;
+
+	function focus() {
+		labelText.style.top = "0px";
+		labelText.style.color = "#EB4F4F";
+		labelText.style.fontSize = "0.9rem";
+	}
+
+	function leave() {
+		if (value.length == 0) {
+			labelText.style.top = "25%";
+			labelText.style.color = "#000000";
+			labelText.style.fontSize = "1rem";
+		}
+	}
 </script>
 
 <div class="passwordInput">
-	<label for="">
+	<label bind:this={labelText}>
 		<slot />
 	</label>
-	<input bind:this={passwordInput} bind:value />
+	<input bind:this={passwordInput} bind:value on:focusin={focus} on:focusout={leave} />
 	<div
 		class="eye"
 		on:click={() => toggleVisibility('click')}
@@ -39,30 +56,45 @@
 
 <style lang="scss">
 	.passwordInput {
+		position: relative;
 		max-width: 480px;
 		width: 100%;
 		min-width: 300px;
+		min-height: 48px;
 		max-height: 48px;
 
 		border: 1px solid #ccc;
 		border-radius: 5px;
 
 		display: flex;
-		align-items: center;
-		justify-content: center;
+		flex-direction: column;
+		align-items: flex-start;
+		justify-content: flex-start;
+
+		label {
+			position: absolute;
+			top: 25%;
+			padding-left: 10px;
+			transition: all 0.2s ease-in-out;
+			font-size: 1rem;
+		}
 
 		input {
 			width: 100%;
-			padding: 0 10px;
+			padding: 20px 10px 0;
+			height: 100%;
 		}
 	}
 
 	.eye {
-		height: 4rem;
+		height: 100%;
 		width: 4rem;
 		cursor: pointer;
 		display: flex;
 		justify-content: center;
 		align-items: center;
+
+		position: absolute;
+		right: 0;
 	}
 </style>
--- src/lib/components/forms/TextInput.svelte
+++ src/lib/components/forms/TextInput.svelte
@@ -1,31 +1,60 @@
 <script lang="ts">
-	export let value: string = '';
+    export let value: string = '';
+
+    let labelText: HTMLElement;
+    let parent: HTMLElement;
+
+    function focus() {
+        labelText.style.top = "0px";
+        labelText.style.color = "#EB4F4F";
+        labelText.style.fontSize = "0.9rem";
+    }
+
+    function leave() {
+        if (value.length == 0) {
+            labelText.style.top = "25%";
+            labelText.style.color = "#000000";
+            labelText.style.fontSize = "1rem";
+        }
+    }
 </script>
 
-<div class="textInput">
-	<label for="">
-		<slot />
-	</label>
-	<input type="text" bind:value />
+<div class="textInput" bind:this={parent}>
+    <label bind:this={labelText}>
+        <slot/>
+    </label>
+    <input type="text" bind:value on:focusin={focus} on:focusout={leave}/>
 </div>
 
 <style lang="scss">
-	.textInput {
-		max-width: 480px;
-		width: 100%;
-		min-width: 300px;
-		height: 48px;
+  .textInput {
+    position: relative;
 
-		border: 1px solid #ccc;
-		border-radius: 5px;
+    max-width: 480px;
+    width: 100%;
+    min-width: 300px;
+    height: 48px;
 
-		display: flex;
-		align-items: center;
-		justify-content: center;
+    border: 1px solid #ccc;
+    border-radius: 5px;
 
-		input {
-			width: 100%;
-			padding: 0 10px;
-		}
-	}
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    label {
+      position: absolute;
+      top: 25%;
+      padding-left: 10px;
+      transition: all 0.2s ease-in-out;
+      font-size: 1rem;
+    }
+
+    input {
+      width: 100%;
+      padding: 20px 10px 0;
+      height: 100%;
+    }
+  }
 </style>
