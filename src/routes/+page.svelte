67fa6ab80baa170d8da10bf971f92652e8be9ae2
H7KZ <74021016+H7KZ@users.noreply.github.com>
2022-11-22 12:18:50 +0100

    feat: start on landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,5 @@
-<div>
-	<h1>Site is being built</h1>
-	<img src="https://c.tenor.com/58DNSt-Lvw0AAAAd/corgi-computer.gif" alt="corgi pc gif" />
-	<h1>Check back soon!</h1>
-</div>
+<script lang="ts">
+	import Landing from "$lib/Landing/Landing.svelte";
+</script>
 
-<style scoped lang="scss">
-	div {
-		height: 100vh;
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		gap: 1rem;
-
-		font-family: 'Comfortaa', sans-serif;
-
-		h1 {
-			font-size: 1.75rem;
-			font-weight: 800;
-
-			@media screen and (min-width: 768px) {
-				font-size: 2.5rem;
-			}
-		}
-
-		img {
-			width: 16rem;
-
-			@media screen and (min-width: 768px) {
-				width: 24rem;
-			}
-		}
-	}
-</style>
+<Landing />
