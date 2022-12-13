7a7345888644a8604a2c21cc537aa9856421ba6e
H7KZ <74021016+H7KZ@users.noreply.github.com>
2022-12-13 13:26:07 +0100

    feat: get started component + fix cards

---
+++ src/lib/components/GetStarted/GetStartedComponent.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+	import GetStartedSection from "./GetStartedSection.svelte";
+
+    export let content: {
+        num: number;
+        title: string;
+        description: string;
+        last?: boolean;
+    }[];
+</script>
+
+<template>
+    <div class="wrap">
+        <div class="sec">
+            <h1>
+                Get started
+            </h1>
+            <div class="sections">
+                {#each content as section}
+                    <GetStartedSection {section} />
+                {/each}
+            </div>
+        </div>
+        
+        <img src="images/started/1.png" alt="pc work">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .wrap {
+        display: flex;
+        gap: 1rem;
+        margin: 0.5rem 1rem;
+        
+        font-family: 'Quicksand', sans-serif;
+
+        @include br-lg {
+            margin: 2rem 4rem;
+        }
+
+        .sec {
+            width: 100%;
+            padding: 0;
+            
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+            gap: 3rem;
+
+            @include br-lg {
+                width: 50%;
+                padding: 0 3rem;
+            }
+
+            h1 {
+                font-size: 3rem;
+                font-weight: 700;
+
+                @include br-md {
+                    font-size: 4rem;
+                }
+            }
+        }
+
+        .sections {
+            margin-left: 3rem;
+            display: flex;
+            flex-direction: column;
+            gap: 3rem;
+
+            @include br-md {
+                margin-left: 6rem;
+            }
+
+            @include br-lg {
+                margin-left: 0;
+            }
+        }
+
+        img {
+            width: 50%;
+            max-height: 880px;
+
+            display: none;
+
+            object-fit: cover;
+
+            border-radius: 4rem;
+
+            @include br-lg {
+                display: block;
+            }
+        }
+    }
+</style>
---
+++ src/lib/components/LandingCards/CardCarousel.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Card from './Card.svelte';
+
+	export let content: {
+		image: string;
+		imageSide: 'left' | 'right';
+		num: number;
+		title: string;
+		text: string;
+	}[];
+</script>
+
+<template>
+	<div id="cards">
+		{#each content as cont}
+			<Card content={cont} />
+		{/each}
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	div {
+		margin: 4rem 0;
+
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+
+        @include br-md {
+            gap: 0;
+        }
+	}
+</style>
