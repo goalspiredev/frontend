d101db41d40ff0c8379be612862c2d75421e58ab
Seb <seb1rblx@gmail.com>
2023-01-19 12:14:44 +0100

    Add notifications mock

--- src/@goalspire/notifications/PushNotifications.ts
+++ src/@goalspire/notifications/PushNotifications.ts
@@ -1,36 +1,43 @@
 async function subscribe() {
-    navigator.serviceWorker.register('./sw.js')
-        .then(registration => {
-            console.log('Service worker registered', registration);
-        })
-        .catch(error => {
-            console.log('Service worker registration failed', error);
-        });
-    const registration = await navigator.serviceWorker.getRegistration();
-    const subscription = await registration?.pushManager.subscribe({
-        userVisibleOnly: true
-    });
-    console.log(subscription);
+	navigator.serviceWorker
+		.register('./sw.js')
+		.then((registration) => {
+			console.log('Service worker registered', registration);
+		})
+		.catch((error) => {
+			console.log('Service worker registration failed', error);
+		});
+	const registration = await navigator.serviceWorker.getRegistration();
+	const subscription = await registration?.pushManager.subscribe({
+		userVisibleOnly: true,
+		applicationServerKey:
+			'BHUqMPRqv8gZjxtQMvV0XcPqOyvOjBOpSJ553SyTUwjudX0bz45tFBZb6R2yy1buMuRztPAI3C0JnrZoSF1rSU4'
+	});
+	console.log(subscription);
+	fetch('http://localhost:3000/register', {
+		method: 'POST',
+		body: JSON.stringify(subscription),
+		headers: {
+			'content-type': 'application/json'
+		}
+	});
 }
 
-async function createExampleNotification() {
-}
+async function createExampleNotification() {}
 
 export class PushNotifications {
-    public static async request() {
-        if (Notification.permission === 'granted') {
-            // subscribe();
-            console.log("User has already granted permission");
-        } else if (Notification.permission !== 'denied') {
-            Notification.requestPermission().then((permission) => {
-                if (permission === 'granted') {
-                    subscribe();
-                    console.log("User has granted permission");
-                } else {
-                    console.log("User has denied permission");
-                }
-            });
-        }
-    }
+	public static async request() {
+		if (Notification.permission === 'granted') {
+			console.log('User has already granted permission');
+		} else if (Notification.permission !== 'denied') {
+			Notification.requestPermission().then((permission) => {
+				if (permission === 'granted') {
+					subscribe();
+					console.log('User has granted permission');
+				} else {
+					console.log('User has denied permission');
+				}
+			});
+		}
+	}
 }
-
--- src/routes/(dashboard)/dashboard/+page.svelte
+++ src/routes/(dashboard)/dashboard/+page.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
 	import Goal from '$components/goals/Goal.svelte';
+	import { onMount } from 'svelte';
+	import { PushNotifications } from '$goalspire/notifications/PushNotifications.js';
+
+	onMount(() => {
+		// const greeting = new Notification('Hi, How are you?', {
+		//     body: 'Have a good day',
+		//     icon: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Macaca_nigra_self-portrait_large.jpg',
+		//     actions: [
+		//         {action: 'Greet', title: 'greet'},
+		//     ]
+		// });
+		//
+		// if ('actions' in Notification.prototype) {
+		//     console.log('This browser supports notification actions.');
+		// } else {
+		//     console.log('This browser does not support notification actions.');
+		// }
+	});
 </script>
 
+<button
+	on:click={() => PushNotifications.request()}
+	style="width: 10px; font-size: 20px; height: 10px">Req</button
+>
 <!--<Goal />-->
